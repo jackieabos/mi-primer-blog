b089041f1e48057e46a39c170ec40c7c91a87cd2
jackie <jackieabos@gmail.com>
2016-12-10 16:55:59 -0300

    Mi app Django Girls, primer commit

---
+++ myvenv/Scripts/django-admin-script.py
@@ -0,0 +1,10 @@
+#!c:\users\vaio-sistemas\djangogirls\myvenv\scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'Django==1.9.11','console_scripts','django-admin'
+__requires__ = 'Django==1.9.11'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('Django==1.9.11', 'console_scripts', 'django-admin')()
+    )
